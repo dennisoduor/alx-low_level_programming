d8d1d47a03e9cd1044ec3e91ff07a17b472886e6
dennisoduor <mrdennisoduor@gmail.com>
2023-02-16 05:38:43 -0800

    writing in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,4 @@
-#!/bin/bash
-echo '#include <stdio.h> int main(void) {    puts("Programming is like building a multilingual puzzle,");    return 0;}' > put.c
+#include <stdio.h> 
+int main(void) {    
+	puts("Programming is like building a multilingual puzzle,");    
+	return 0;}
