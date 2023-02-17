3afa7f413c6aa58de14397ee1d337f38543ce67e
dennisoduor <mrdennisoduor@gmail.com>
2023-02-17 02:39:18 -0800

    using for..loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Dennis Oduor
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n);
+
+//	printf("\n");
+
+	return (0);
+}
