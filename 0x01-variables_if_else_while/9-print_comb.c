88c78aca001c6fc44689076ee2e57239c0ec03d2
dennisoduor <mrdennisoduor@gmail.com>
2023-02-17 09:09:47 -0800

    use for loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 /*
  * File: 9-print_comb.c
- * Auth: Brennan D Baraban
+ * Auth: dennis Oduor
  */
 
 #include <stdio.h>
@@ -19,7 +19,6 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
