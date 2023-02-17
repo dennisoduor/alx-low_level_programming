00b36826a4cf1de5071d20a6ec9b07213e7888d0
dennisoduor <mrdennisoduor@gmail.com>
2023-02-17 00:38:15 -0800

    using if..statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints "if the number is greater than 0: is positive
+*			if the number is 0: is zero
+*			if the number is less than 0: is negative
+*			followed by a new line.
+*			Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
