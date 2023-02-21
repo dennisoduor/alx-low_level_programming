212b3b182140f5756b0bddeeee90e85dfd545324
dennisoduor <mrdennisoduor@gmail.com>
2023-02-21 02:37:49 -0800

    print alpherbets to lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * file: 1-alphabet.c
+ * auth: Dennis Oduor
+ */
+# include "main.h"
+/**
+ *  main- Write a function that prints the alphabet,
+ *  in lowercase, followed by a new line.
+ *  Return: Always 0.
+*/
+int main(void)
+{
+	ret = print_alphabet();
+	_putchar(ret);
+	_putchar('\n');
+	return (0);
+}
