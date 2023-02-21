6034acf73c3d3ba9ce33217b44fe5699df872e14
dennisoduor <mrdennisoduor@gmail.com>
2023-02-21 00:22:29 -0800

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/*
+ * * file: 0-putchar.c
+ * AUTH: Denni Oduor
+ */
+# include <stdio.h>
+/**
+ *  main- Write a program that prints _putchar, followed by a new line.
+ *
+ *  Return: Always 0.
+*/
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}
