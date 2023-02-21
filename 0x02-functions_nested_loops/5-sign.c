9087381ac691d0b258e65f9b32c67b9ce7da71e1
dennisoduor <mrdennisoduor@gmail.com>
2023-02-21 06:35:02 -0800

    nested loop

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,5 @@
 /*
  * file: 5-sign.c
- * file: 3-islower.c
  * auth: Dennis Oduor
  */
 # include "main.h"
@@ -16,11 +15,11 @@ int print_sign(int n)
 {
 	if (n > 0)
 		_putchar('+');
-	return (1);
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
 	else if (n < 0)
 		_putchar('-');
-	return (-1);
-	else
-	_putchar('0');
-	return (0);
+		return (-1);
 }
