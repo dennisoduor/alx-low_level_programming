5865cc00864ff0d3bda702a72f793cd76afbe901
dennisoduor <mrdennisoduor@gmail.com>
2023-02-21 02:48:49 -0800

    print alpherbets to lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,12 @@
  *  in lowercase, followed by a new line.
  *  Return: Always 0.
 */
-int main(void)
+void print_alphabet(void)
 {
-	_putchar(print_alphabet(void));
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+
 	_putchar('\n');
-	return (0);
 }
