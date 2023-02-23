1dfc4e3a80dea1efb40d2d3bb94052db559dde2e
dennisoduor <mrdennisoduor@gmail.com>
2023-02-23 02:31:58 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
