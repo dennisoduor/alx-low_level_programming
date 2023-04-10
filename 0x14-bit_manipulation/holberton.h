b34fd9bb65db25fd725b4e12077ebe42c05eb60d
dennisoduor <mrdennisoduor@gmail.com>
2023-04-10 12:59:40 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,14 +1,19 @@
-#ifndef _BIT_MANIP_
-#define _BIT_MANIP_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-int _strlen(const char *s);
+/*
+ * File: holberton.h
+ * AUth: Dennis Oduor
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
+
 unsigned int binary_to_uint(const char *b);
-int _putchar(char c);
 void print_binary(unsigned long int n);
-void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
-#endif
+#endif /* HOLBERTON_H */
